a70dd77893cfbde23cdea3f76ce58a20d57bc211
namalfernandolk <namalakit@gmail.com>
2016-08-04 20:17:05 +0530

    First commit

---
+++ src/main/java/sabre/SabreAirBook.java
@@ -0,0 +1,5 @@
+package sabre;
+
+public class SabreAirBook {
+
+}
---
+++ src/main/java/sabre/SabreLowFareSearch.java
@@ -0,0 +1,19 @@
+package sabre;
+
+public class SabreLowFareSearch {
+
+	public static void main(String[] args) {
+	
+		SessionCreate  sessionCreate = new SessionCreate();
+		
+		String sessionId = sessionCreate.getSessionId(sessionCreate.invokeRequest(sessionCreate.buildRequest()));
+		System.out.println(sessionId);
+		
+		// build the LowFareSearch with the sessionID
+		// invoke the request
+		// decode the response
+		
+
+	}
+	
+}
